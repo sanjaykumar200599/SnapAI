f319d728937800172fc345a28e204c245cc6cff5
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-07-25 17:20:11 +0530

    Clerk is added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import { FloatingShapes } from "@/components/floating-shapes";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const inter= Inter({subsets:['latin']})
@@ -17,20 +19,24 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
-      >
+      > 
          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            <Header/>
-              <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
-                <FloatingShapes />
-                <Toaster richColors />
-              {children}</main>
-            
+              <ClerkProvider>
+              <ConvexClientProvider>
+                <Header/>
+                  <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+                    <FloatingShapes />
+                    <Toaster richColors />
+                  {children}</main>
+                </ConvexClientProvider>
+                </ClerkProvider>
           </ThemeProvider>
+         
       </body>
     </html>
   );
