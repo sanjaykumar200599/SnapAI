50fd937910f9ec71c622da26e1744fbc11873637
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-07-25 16:45:31 +0530

    Connected to database

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      Hello how are you<br/>
-      <Button variant="primary"> Hello</Button>
+     
 
     </div>
   );
